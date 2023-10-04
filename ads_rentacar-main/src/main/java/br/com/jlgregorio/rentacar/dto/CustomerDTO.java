8670654a8416720e532c37a7a2684d8e66be2896
Lucimeire <lucimeirenascimento0@gmail.com>
2023-10-04 09:59:54 -0300

    commit

---
+++ ads_rentacar-main/src/main/java/br/com/jlgregorio/rentacar/dto/CustomerDTO.java
@@ -0,0 +1,34 @@
+package br.com.jlgregorio.rentacar.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class CustomerDTO {
+
+    private int id;
+
+    @NotBlank
+    @Size(min = 1, max = 50)
+    private String firstName;
+
+    @NotBlank
+    @Size(min = 1, max = 50)
+    private String lastName;
+
+    @NotBlank
+    @Size(min = 5, max = 50)
+    private String city;
+
+    public String getFullName(){
+        return this.firstName + " " + this.lastName;
+    }
+
+}
